3871ea528f92cd157e3d0efd4e5c3dfb3cca64c0
Dzulfikar Lintang <setyajilintang@gmail.com>
2025-11-14 15:26:02 +0700

    penambahan cors

--- src/Server.jsx
+++ src/Server.jsx
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://backend-manpro.web.id";
 
 export async function apiFetch(endpoint, options = {}) {
   const token = localStorage.getItem("token");
