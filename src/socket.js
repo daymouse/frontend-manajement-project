dd58fe41ae818c9af98fbde2b62cf1421e06cc6d
Dzulfikar Lintang <setyajilintang@gmail.com>
2025-11-17 21:41:31 +0700

    penyesuaian di socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 export const socket = io(import.meta.env.VITE_API_URL || "https://backend-manpro.web.id", {
-  transports: ["websocket"],
+  transports: ["polling", "websocket"],
   withCredentials: true,
 });
